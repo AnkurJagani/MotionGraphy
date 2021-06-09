a470f58bca5466a55e7fd96dc95e699339d2cfeb
OVES Mobile <AnkurJagani@users.noreply.github.com>
2021-06-09 18:38:40 +0530

    basic slide animation in main activity and transition basic

--- app/src/main/java/com/ankur/motiongraphy/MainActivity.java
+++ app/src/main/java/com/ankur/motiongraphy/MainActivity.java
@@ -1,15 +1,51 @@
 package com.ankur.motiongraphy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.Slide;
+import androidx.transition.TransitionManager;
 
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private View cardView;
+    private View imgAnimation;
+    private View btnClick;
+    private View tvTest;
+    private boolean isVisible = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_transition);
+        cardView = findViewById(R.id.cardView);
+        imgAnimation = findViewById(R.id.imgAnimation);
+        tvTest = findViewById(R.id.tvTest);
+        btnClick = findViewById(R.id.btnClick);
+        btnClick.setOnClickListener(this);
     }
-    //
+
+    @Override
+    public void onClick(View view) {
+        if (view.getId() == R.id.btnClick) {
+            btnClickListener();
+        }
+    }
+
+    private void btnClickListener() {
+        ViewGroup viewGroup = findViewById(android.R.id.content);
+        if (isVisible) {
+            TransitionManager.beginDelayedTransition(viewGroup, new Slide(Gravity.TOP));
+            cardView.setVisibility(View.INVISIBLE);
+            tvTest.setVisibility(View.INVISIBLE);
+        } else {
+            TransitionManager.beginDelayedTransition(viewGroup, new Slide(Gravity.TOP));
+            cardView.setVisibility(View.VISIBLE);
+            tvTest.setVisibility(View.VISIBLE);
+        }
+        isVisible = !isVisible;
+    }
 }
---
+++ app/src/main/java/com/ankur/motiongraphy/TransitionActivity.java
@@ -0,0 +1,40 @@
+package com.ankur.motiongraphy;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.transition.Scene;
+import androidx.transition.Slide;
+import androidx.transition.Transition;
+import androidx.transition.TransitionManager;
+
+public class TransitionActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Scene firstScene;
+    private Scene nextScene;
+    private Scene currentScene;
+    private Transition transition;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition);
+        FrameLayout flContainer = findViewById(R.id.flContainer);
+        // Step 1: Create a Scene object for both the starting and ending layout
+        firstScene = Scene.getSceneForLayout(flContainer, R.layout.layout_scene_first, this);
+        // Step 2: Create a Transition object to define what type of animation you want
+
+        firstScene.enter();
+        currentScene = firstScene;
+    }
+
+    @Override
+    public void onClick(View view) {
+        // Step 3: Call TransitionManager.go() to run animation
+    }
+
+}
